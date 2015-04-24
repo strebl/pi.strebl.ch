81bbc0575c5c28aa882e9fa226cb7cc801eae9a2
Manuel Strebel <manuel_st@msn.com>
2015-04-25 00:25:31 +0200

    migrate database before test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,19 +2,32 @@
 
 class ExampleTest extends TestCase {
 
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->prepareForTests();
+	}
+
+	private function prepareForTests()
+	{
+		Artisan::call('migrate');
+	}
+
 	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
+	 * @test
 	 */
-	public function testBasicExample()
+	public function it_returns_status_code_200_on_the_homepage()
 	{
 		$response = $this->call('GET', '/');
 
 		$this->assertEquals(200, $response->getStatusCode());
 	}
 
-	public function testDatabaseEnv()
+	/**
+	 * @test
+	 */
+	public function it_has_the_correct_database_for_testing()
 	{
 		$this->assertEquals(':memory:', env('SQLITE_DATABASE'));
 	}
