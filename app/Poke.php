b4c26d6a7f9c785b2e8df804d964674402ee49a0
Manuel Strebel <manuel_st@msn.com>
2015-05-22 22:20:28 +0200

    create poke model

---
+++ app/Poke.php
@@ -0,0 +1,8 @@
+<?php namespace PiFinder;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Poke extends Model
+{
+    protected $fillable = ['ip', 'mac'];
+}
---
+++ database/migrations/2015_05_22_215117_create_pokes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePokesTable extends Migration
+{
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('pokes', function(Blueprint $table)
+		{
+            $table->increments('id');
+            $table->string('ip', 15);
+            $table->string('mac', 7);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('pokes');
+	}
+
+}
