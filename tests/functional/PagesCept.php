0c6c9a37af5894806a927f7e24f0e529a2b64200
Manuel Strebel <manuel_st@msn.com>
2015-05-22 22:23:57 +0200

    test getting started and stats page

---
+++ tests/functional/PagesCept.php
@@ -0,0 +1,17 @@
+<?php 
+$I = new FunctionalTester($scenario);
+$I->wantTo('visit all pages');
+
+$I->amGoingTo('visit the getting started page');
+
+$I->amOnPage('/getting-started');
+
+$I->seeResponseCodeIs(200);
+$I->see('Introduction');
+$I->see('Installation');
+
+$I->amGoingTo('visit the statistics page');
+
+$I->amOnPage('/stats');
+
+$I->seeResponseCodeIs(200);
