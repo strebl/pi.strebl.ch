9c9f6a2f704907d0f1858a89b6e345e6bc5598e8
Manuel Strebel <manuel_st@msn.com>
2015-05-23 11:22:28 +0200

    Revert "remove inspire command"
    This reverts commit 8fac8ff83805b219966915d29f08244050549a05.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        'PiFinder\Console\Commands\Inspire',
         'PiFinder\Console\Commands\FlushOldDevices',
     ];
 
