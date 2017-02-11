b17b2c6ad6bb3476463aa1ec180c9f61b9f91994
Manuel Strebel <manuel_st@msn.com>
2017-02-11 16:12:58 +0000

    Apply fixes from StyleCI

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -19,7 +19,7 @@ abstract class DuskTestCase extends BaseTestCase
      */
     public static function prepare()
     {
-        if(! env('TRAVIS')) {
+        if (! env('TRAVIS')) {
             static::startChromeDriver();
         }
     }
@@ -31,7 +31,7 @@ abstract class DuskTestCase extends BaseTestCase
      */
     protected function driver()
     {
-        if(env('TRAVIS')) {
+        if (env('TRAVIS')) {
             return RemoteWebDriver::create(
                 'http://localhost:9515', DesiredCapabilities::phantomjs()
             );
